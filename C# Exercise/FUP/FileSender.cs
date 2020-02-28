248b9ba6823d03cf211ee37de781db898f184872
Myohalang <myohalang@naver.com>
2020-02-28 23:53:49 +1000

    changes

---
+++ C# Exercise/FUP/FileSender.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using FUP;
+using static System.Console;
+
+namespace FileSender
+{
+    class MainApp
+    {
+        const int CHUNK_SIZE = 4096;
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                WriteLine($"사용법 : {Process.GetCurrentProcess().ProcessName}");
+
+                return;
+            }
+
+            string serverIp = args[0];
+            const int serverPort = 5425;
+            string filePath = args[1];
+
+            try
+            {
+                IPEndPoint clientAddress = new IPEndPoint(0, 0);
+                IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+                WriteLine($"클라이언트 : {clientAddress.ToString()}, 서버 : {serverAddress.ToString()}");
+
+                uint msgId = 0;
+
+                Message reqMsg = new Message();
+                reqMsg.Body = new BodyRequest()
+                {
+                    FILESIZE = new FileInfo(filePath).Length,
+                    FILENAME = System.Text.Encoding.Default.GetBytes(filePath)
+                };
+
+                reqMsg.Header = new Header()
+                {
+                    MSGID = msgId++,
+                    MSGTYPE = CONSTANTS.REQ_FILE_SEND,
+                    BODYLEN = (uint)reqMsg.Body.GetSize(),
+                    FRAGMENTED = CONSTANTS.NOT_FRAGMENTED,
+                    LASTMSG = CONSTANTS.LASTMSG,
+                    SEQ = 0
+                };
+
+                TcpClient client = new TcpClient(clientAddress);
+                client.Connect(serverAddress);
+
+                NetworkStream stream = client.GetStream();
+
+                MessageUtil.Send(stream, reqMsg);
+
+                Message rspMsg = MessageUtil.Receive(stream);
+
+                if (rspMsg.Header.MSGTYPE != CONSTANTS.REP_FILE_SEND)
+                {
+                    WriteLine($"정상적인 서버 응답이 아닙니다. {rspMsg.Header.MSGTYPE}");
+
+                    return;
+                }
+
+                if (((BodyResponse)rspMsg.Body).RESPONSE == CONSTANTS.DENIED)
+                {
+                    WriteLine("서버에서 파일 전송을 거부했습니다.");
+
+                    return;
+                }
+
+                using (Stream fileStream = new FileStream(filePath, FileMode.Open))
+                {
+                    byte[] rbytes = new byte[CHUNK_SIZE];
+
+                    long readValue = BitConverter.ToInt64(rbytes, 0);
+
+                    int totalRead = 0;
+                }
+            }
+        }
+    }
+}
