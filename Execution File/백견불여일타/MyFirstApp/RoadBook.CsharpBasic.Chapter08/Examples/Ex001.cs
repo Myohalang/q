ed5cf998c3df70d15b9a8f90448d234b8b698960
Myohalang <myohalang@naver.com>
2020-03-21 11:32:14 +1000

    changes

---
+++ Execution File/백견불여일타/MyFirstApp/RoadBook.CsharpBasic.Chapter08/Examples/Ex001.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace RoadBook.CsharpBasic.Chapter08.Examples
+{
+    class Ex001
+    {
+        private readonly string currentDirectory = Environment.CurrentDirectory;
+
+        public void Run()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(currentDirectory + @"/data");
+
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+                WriteLine("디렉토리가 생성되었습니다.");
+            }
+
+            WriteLine($"생성 경로 : {directoryInfo.FullName}");
+        }
+    }
+}
---
+++ Execution File/백견불여일타/MyFirstApp/RoadBook.CsharpBasic.Chapter08/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RoadBook.CsharpBasic.Chapter08
+{
+    class Program
+    {
+        static void Main()
+        {
+            Examples.Ex001 Ex = new Examples.Ex001();
+            Ex.Run();
+        }
+    }
+}
