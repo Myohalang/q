cd76d13be94b70aca4213b2299fba94b1482b525
Myohalang <myohalang@naver.com>
2020-03-22 17:25:32 +1000

    changes

--- Execution File/백견불여일타/MyFirstApp/RoadBook.CsharpBasic.Chapter08/Examples/Ex004.cs
+++ Execution File/백견불여일타/MyFirstApp/RoadBook.CsharpBasic.Chapter08/Examples/Ex004.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.IO;
+using System.Data.SqlClient;
 using static System.Console;
 
 namespace RoadBook.CsharpBasic.Chapter08.Examples
 {
     class Ex004
     {
+        private readonly string correntDirectory = Environment.CurrentDirectory;
+        private readonly string connectionStr = string.Format($"Data Source = {"127.0.0.1"}, {1433}; Initial Catalog = {"testdb"}; User ID = {"sa"}; Password = {"1234"}");
+
         public void Run()
+        {
+
+        }
+
+        private void CheckedDirectory()
+        {
+
+        }
+
+        private void TryConnectToDatabase()
         {
 
         }
