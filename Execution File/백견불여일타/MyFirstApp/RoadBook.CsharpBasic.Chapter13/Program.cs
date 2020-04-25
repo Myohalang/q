6a2ddf26bc60fc28ad920ca68c1f52fa36b5f650
Myohalang <myohalang@naver.com>
2020-04-25 20:14:05 +1000

    changes

--- Execution File/백견불여일타/MyFirstApp/RoadBook.CsharpBasic.Chapter13/Program.cs
+++ Execution File/백견불여일타/MyFirstApp/RoadBook.CsharpBasic.Chapter13/Program.cs
@@ -1,3 +1,20 @@
 ﻿using log4net;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace RoadBook.CshrpBasic.Chapter13
+{
+    class program
+    {
+        static ILog log = LogManager.GetLogger("Program");
+
+        static void Main(string[] args)
+        {
+            log.Debug("디버그 메시지");
+            log.Info("정보 메시지");
+            log.Warn("경고 메시지");
+            log.Error("에러 메시지");
+            log.Fatal("치명적인 에러 메시지");
+        }
+    }
+}
